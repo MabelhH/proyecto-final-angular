4a4fb99e5cbcbed45a99ae3044026b11ddb100e3
clara23 <clarahuanaco20@gmail.com>
2023-01-30 15:44:17 -0500

    actulisacion02

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const Environment = {
+    firebase: {
+     apiKey: "AIzaSyDWBKAc1Q2LKV3QE7Rk1TZ715avOa3xwL8",
+     authDomain: "proyecto-angular-final.firebaseapp.com",
+     projectId: "proyecto-angular-final",
+     storageBucket: "proyecto-angular-final.appspot.com",
+     messagingSenderId: "115053075519",
+     appId: "1:115053075519:web:f3e44c352db18bbd8aec8a"
+    },
+}
+
